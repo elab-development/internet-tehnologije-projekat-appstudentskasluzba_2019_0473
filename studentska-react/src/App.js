17809aaab924afeb58b4caa90670b738fd52bca7
Djordje Sudimac <djordjedjordjex@gmail.com>
2024-02-01 00:30:05 +0100

    Kreirana i pocetna strana sa modalom za vise informacija

--- studentska-react/src/App.js
+++ studentska-react/src/App.js
@@ -1,13 +1,21 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './komponente/footer/Footer';
 import Navbar from './komponente/nav/Navbar';
+import Pocetna from './komponente/pocetna/Pocetna';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route 
+        path='/'
+        element={<Pocetna/>}>
+        </Route>
+        
+      </Routes>
       <Footer/>
       </BrowserRouter>
       
