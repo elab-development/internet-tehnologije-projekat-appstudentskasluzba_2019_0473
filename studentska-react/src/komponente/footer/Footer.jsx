07165b401ff6d196a561ca5658b815f2767e5257
djordjesudimac <djordjedjordjex@gmail.com>
2024-06-23 23:02:29 +0200

    Izmene u tabeli predmeta

--- studentska-react/src/komponente/footer/Footer.jsx
+++ studentska-react/src/komponente/footer/Footer.jsx
@@ -6,14 +6,14 @@ const Footer = () => {
     <footer className="footer">
       <div className="contact-info">
         <div className="contact-details">
-          <p>Elektronska posta: studentska@fon.bg.ac.rs</p>
+          <p>Elektronska pošta: studentska@fon.bg.ac.rs</p>
           <div className='telefoni'>
           <h4>Telefoni:</h4>
           <p>+381 11 3950 809  Službenik za diplomske i završne radove </p>
           <p>+381 11 3950 811  Službenik za ispite</p>
           <p>+381 11 3950 810  Službenik za studentska pitanja</p>
           </div>
-          <p>Radno vreme: 10-13h </p>
+          <p>Radno vreme: 11-13h </p>
         </div>
       </div>
     </footer>
